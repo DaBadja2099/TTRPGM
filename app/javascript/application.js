27dd0a9bfc2c3c4ff05bbd69c0a5239169e0076e
Alexandre Ribeiro <aleribeiro.dev@gmail.com>
2024-08-30 11:20:05 -0300

    Bootstrap, bootswatch and fontawesome installed, devise views created, homepage OK

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap';
+
+// Import Bootstrap JS from the CDN
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js';
+
+// Import Bootstrap Bundle JS from the CDN
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
